6ef1de0c18b7e0279ab52a50875471f22b9a384f
Vladimir <103881264+sxd993@users.noreply.github.com>
2025-10-25 18:56:33 +0500

    кнопка добавления транзакции

--- frontend/src/pages/expenses/ExpensesPage.tsx
+++ frontend/src/pages/expenses/ExpensesPage.tsx
@@ -1,6 +1,9 @@
 import { ExpensesFilters } from "@/features/expenses-filters";
 import { useScrollToTop } from "@/shared/hooks/useScrollToTop";
+import { Button } from "@/shared/ui/Button";
 import { ExpensesOverview, ExpensesList } from "@/widgets/expenses";
+import { Plus } from "lucide-react";
+import { NavLink } from "react-router-dom";
 
 export const ExpensesPage = () => {
     useScrollToTop();
@@ -8,7 +11,16 @@ export const ExpensesPage = () => {
     return (
         <div className="flex flex-col max-w-3xl mx-auto pt-5 pb-20 px-4">
             <ExpensesOverview />
-            <ExpensesFilters />
+            <div className="flex justify-between items-center mt-4 mb-4 p-4">
+                <ExpensesFilters />
+                <NavLink to={"add-expense"}>
+                    <Button
+                        size="sm"
+                        leftIcon={<Plus />}
+                        title="Добавить транзакцию"
+                    />
+                </NavLink>
+            </div>
             <ExpensesList />
         </div>
     );
--- frontend/src/widgets/expenses/ExpensesList/ui/ExpenseListEmpty.tsx
+++ frontend/src/widgets/expenses/ExpensesList/ui/ExpenseListEmpty.tsx
@@ -1,6 +1,6 @@
 export const ExpensesListEmpty = () => {
     return (
-        <div>
+        <div className="flex justify-center mt-2 mb-2">
             <h1>У вас нет транзакций</h1>
         </div>
     )
